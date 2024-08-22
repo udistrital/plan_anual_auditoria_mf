26b2e228ac6815fa18a82175eb7dbd927fb3425d
CristianCGutierrezG <cris123.gut@gmail.com>
2024-08-21 19:56:34 -0500

    feat: Implementacion vista consulta plan de auditoria udistrital/sisifo_documentacion#128

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GestionAuditoriaComponent } from './modules/gestion-auditoria/gestion-auditoria.component';
+import { ConsultaPlanAnualAuditoriaComponent } from './modules/consulta-plan-anual-auditoria/consulta-plan-anual-auditoria.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   {
     path:"gestion-auditoria",
     component: GestionAuditoriaComponent
+  },
+  {
+    path:"consultar-plan",
+    component: ConsultaPlanAnualAuditoriaComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/plan-auditoria/"}]
 })
 export class AppRoutingModule { }
