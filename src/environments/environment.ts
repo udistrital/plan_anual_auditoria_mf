f922371e941baf18f8c0984e37e3a35b78de7734
alexhrr <alexh.2401@hotmail.com>
2024-10-29 20:38:51 -0500

    feat: Se elimina la variable de entorno LAMBDA_SERVICE de environment udistrital/sisifo_documentacion#234

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
     GESTOR_DOCUMENTAL_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
-    LAMBDA_SERVICE: 'http://localhost:3000',
     PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
 
 
