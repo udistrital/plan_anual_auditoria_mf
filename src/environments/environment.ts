e8b652e5c3bc80a6a0f97d7d34a16a8f6695f061
alexhrr <alexh.2401@hotmail.com>
2024-11-19 17:17:30 -0500

    feat: Se agrega el id del plan de auditoria al modal de cargue masivo udistrital/sisifo_documentacion#281

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,11 @@ export const environment = {
     'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
   PARAMETROS_SERVICE:
     'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
-  PLAN_ANUAL_AUDITORIA_SERVICE: 'http://localhost:3000',
+  PLAN_ANUAL_AUDITORIA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_anual_auditoria_crud/v1',
   TERCEROS_SERVICE:
     'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
+
+  LAMBDA_SERVICE: 'http://localhost:3001',
 
   PLAN_ESTADO: {
     EN_BORRADOR_ID: 6790,
