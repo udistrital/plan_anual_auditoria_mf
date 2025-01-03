8e83896f6cd4498de098efddd821f61e8a93aad3
edwargl7 <07edwargl@gmail.com>
2025-01-03 15:08:01 -0500

    fix: actualización variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,14 @@
 export const environment = {
   production: false,
-  GESTOR_DOCUMENTAL_SERVICE:
-    "https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/",
-  PARAMETROS_SERVICE:
-    "https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/",
+
+  AUTENTICACION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/",
+  GESTOR_DOCUMENTAL_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/",
+  PARAMETROS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/",
   // PLAN_ANUAL_AUDITORIA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_anual_auditoria_crud/v1/",
   PLAN_ANUAL_AUDITORIA_SERVICE: "http://localhost:8080/",  
   PLAN_ANUAL_AUDITORIA_MID: "http://localhost:3001/",
 
-  TERCEROS_SERVICE:
-    "https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/",
-
-  AUTENTICACION_MID_SERVICE:
-    "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/",
+  TERCEROS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/",
 
   PLAN_ESTADO: {
     EN_BORRADOR_ID: 6790,
