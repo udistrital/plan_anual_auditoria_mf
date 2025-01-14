b86f7437245f17cdc516466a0ea6814066669bfe
Heiner Edwar Granados Leal <50187485+edwargl7@users.noreply.github.com>
2025-01-14 17:05:54 -0500

    fix: Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
   AUTENTICACION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/",
   GESTOR_DOCUMENTAL_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1",
   PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
-  PLAN_ANUAL_AUDITORIA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_anual_auditoria_crud/v1',
+  PLAN_ANUAL_AUDITORIA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_anual_auditoria_crud/v1/',
   PLAN_ANUAL_AUDITORIA_MID: "http://localhost:3001/",
   TERCEROS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/",
   
