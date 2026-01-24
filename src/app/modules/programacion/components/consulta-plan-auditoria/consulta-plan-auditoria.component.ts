74ffe57714d7290bbe4fbbd60efc66ab9b9f0c5b
Nicolas Sabogal <ndsabogalv@gmail.com>
2026-01-24 14:49:22 -0500

    feat: añadir códigos de color por estado a la lista de planes anuales de auditoría; refactor: mostrar lista en orden descendiente de vigencia_id udistrital/sisifo_documentacion#451

--- src/app/modules/programacion/components/consulta-plan-auditoria/consulta-plan-auditoria.component.ts
+++ src/app/modules/programacion/components/consulta-plan-auditoria/consulta-plan-auditoria.component.ts
@@ -15,6 +15,7 @@ import { MatPaginator } from "@angular/material/paginator";
 import { ModalVerDocumentosPlanComponent } from "./modal-ver-documentos-plan/modal-ver-documentos-plan.component";
 import { RolService } from "src/app/core/services/rol.service";
 import { accionesProgramacion } from "src/app/shared/utils/accionesPorRolYEstado";
+import emojiColorPorPrefijoEstado from "src/app/shared/utils/colorPorPrefijoEstado";
 
 @Component({
   selector: "app-consulta-plan-auditoria",
@@ -120,7 +121,7 @@ export class ConsultaPlanAuditoriaComponent implements OnInit {
   cargarPlanesAuditoria(limit: number, offset: number) {
     const EN_BORRADOR_ID = environment.PLAN_ESTADO.EN_BORRADOR_ID;
     this.PlanAnualAuditoriaMid.get(
-      `plan-auditoria?query=&limit=${limit}&offset=${offset}`
+      `plan-auditoria?sortby=vigencia_id&order=desc&query=&limit=${limit}&offset=${offset}`
     ).subscribe(
       (res) => {
         if (!res?.Data) return;
@@ -141,6 +142,7 @@ export class ConsultaPlanAuditoriaComponent implements OnInit {
             creadoPor: item.creado_por_nombre ?? "Sin asignar",
             vigencia: item.vigencia_nombre ?? "No encontrada",
             fechaCreacion: item.fecha_creacion ?? "No encontrada",
+            colorEstado: this.escogerEmojiColorEstado(item.estado?.estado_nombre ?? "Borrador"),
             estado: item.estado?.estado_nombre ?? "Borrador",
             estadoId,
             acciones,
@@ -154,6 +156,19 @@ export class ConsultaPlanAuditoriaComponent implements OnInit {
         );
       }
     );
+  }
+
+  /**
+   * Chooses an emoji color icon based on the prefix of the state name.
+   * @param {string} estado The full state name.
+   * @returns {string} The corresponding emoji color icon (see {@link emojiColorPorPrefijoEstado}) or ⚪ if no match is found.
+   */
+  escogerEmojiColorEstado(estado: string): string {
+    for (const prefijo in emojiColorPorPrefijoEstado)
+      if (estado.startsWith(prefijo))
+        return emojiColorPorPrefijoEstado[prefijo];
+
+    return "⚪"; // Default icon if no prefix matches
   }
 
   crearPlan() {
