a0f4e85c1e97ef1a8cc45f48fbd7c4ef26851136
jupmorenor <jupmorenor@gmail.com>
2026-02-09 12:49:36 -0500

    feat: Ajustes de estados en el flujo de las fases de planeación y programación. udistrital/sisifo_documentacion#483

--- src/app/modules/planeacion/components/auditorias-internas/revision-documentos/revision-documentos.utilidades.ts
+++ src/app/modules/planeacion/components/auditorias-internas/revision-documentos/revision-documentos.utilidades.ts
@@ -3,15 +3,15 @@ import { environment } from "src/environments/environment";
 export const rolesAprobacion: { [key: string]: any } = {
   jefe: {
     estadoAprobacion: [
-      environment.AUDITORIA_ESTADO.APROBADO_PROGRAMA_POR_JEFE_ID,
-      environment.AUDITORIA_ESTADO.EN_REVISIÓN_POR_AUDITADO_ID,
+      environment.AUDITORIA_ESTADO.PLANEACION.APROBADO_PROGRAMA_JEFE,
+      environment.AUDITORIA_ESTADO.PLANEACION.REVISION_PROGRAMA_AUDITADO,
     ],
     preguntaAprobacion: "¿Está seguro(a) de aprobar y enviar auditoría?",
     mensajeAprobacion: "La auditoría fue enviada al auditado (a) responsable",
     botonAprobacion: "Aprobar y enviar a Auditado",
   },
   auditado: {
-    estadoAprobacion: environment.AUDITORIA_ESTADO.APROBADO_POR_AUDITADO_ID,
+    estadoAprobacion: environment.AUDITORIA_ESTADO.PLANEACION.APROBADO_PROGRAMA_AUDITADO,
     preguntaAprobacion: "¿Está seguro(a) de firmar y enviar auditoría?",
     mensajeAprobacion: "La auditoria fue enviada al auditor",
     botonAprobacion: "Firmar y enviar a Auditor",
