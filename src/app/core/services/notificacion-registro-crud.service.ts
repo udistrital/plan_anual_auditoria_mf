808f179dd3fd73bd9e914ae8e2806855fa0fc224
JorgeLo1 <jeyle222@gmail.com>
2026-02-22 22:52:54 -0500

    feat: se añade servicio notificacion-registro-crud e integración de trazabilidad de notificaciones en flujo de envío de aprobación PAA udistrital/sisifo_documentacion#502

---
+++ src/app/core/services/notificacion-registro-crud.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { RequestManager } from '../managers/requestManager';
+import { Observable } from 'rxjs';
+
+export const NOTIFICACION_REGISTRO_ENDPOINT = 'notificacion-registro';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacionRegistroCrudService {
+
+  constructor(private requestManager: RequestManager) {
+    this.requestManager.setPath('PLAN_ANUAL_AUDITORIA_SERVICE');
+  }
+  
+  post(data: any): Observable<any> {
+    this.requestManager.setPath('PLAN_ANUAL_AUDITORIA_SERVICE');
+    return this.requestManager.post(NOTIFICACION_REGISTRO_ENDPOINT, data);
+  }
+
+}
