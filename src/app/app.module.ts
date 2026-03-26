af58a5fb49e1c10ce42522b71881ff7d3ca28a9c
edwargl7 <07edwargl@gmail.com>
2026-03-26 07:29:16 -0500

    feat: actualización angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,18 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PlanAnualAuditoriaService } from "src/app/core/services/plan-anual-auditoria.service";
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withInterceptors,
-} from "@angular/common/http";
+import { provideHttpClient, withInterceptors, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { SpinnerInterceptor } from "./core/intercerptors/spinner.interceptor";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SpinnerComponent } from "./shared/elements/components/spinner/spinner.component";
 
-@NgModule({
-  declarations: [AppComponent, SpinnerComponent],
-  imports: [
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [
-    PlanAnualAuditoriaService,
-    provideHttpClient(withInterceptors([SpinnerInterceptor])),
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, SpinnerComponent],
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule], providers: [
+        PlanAnualAuditoriaService,
+        provideHttpClient(withInterceptors([SpinnerInterceptor])),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
